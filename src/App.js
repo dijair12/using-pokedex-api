6426be4bd7d6ff1d765ac85401ac03f61ce30b37
Dijair Ribeiro <dijair.ribeiro@qsaude.com.br>
2021-05-31 18:52:23 -0300

    creating card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import api from './services/api';
+
 import './App.css';
+import Card from './components/Card/Card';
 
 function App() {
+  const [treecko, setTreecko] = useState([]);
+
+  function getListPokemon(){
+    const list = []
+    api.get('?limit=9&offset=40')
+      .then(response => {
+        // list.push(response.data.results.name);
+        console.log(response.data.results.name)
+      })
+  }
+  getListPokemon();
+
+  useEffect(()=>{
+    function pokeApiList(){
+      api.get('/treecko')
+      .then(response =>{
+        setTreecko(response.data);
+        // console.log(response.data);
+      })
+    }
+
+    pokeApiList();    
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card 
+        id={treecko.id}
+        name={treecko.name}
+      />
+      <Card 
+        id={treecko.id}
+        name={treecko.name}
+      />
+      <Card 
+        id={treecko.id}
+        name={treecko.name}
+      />
     </div>
   );
 }
---
+++ src/components/Card/Card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './Card.module.scss';
+
+export default function Card({
+  name,
+  id
+}){
+  return(
+    <div className={styles.cardContainer}>
+      <div className={styles.cardChild}>
+        <img alt='robots' src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`} />
+        <h2>{name}</h2>
+        <p>{id}</p>
+      </div>
+    </div>
+  )
+}
